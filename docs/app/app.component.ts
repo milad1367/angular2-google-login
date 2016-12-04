503b7235d678c19b4437699dc2c9687724041c06
miladas01 <miladasghari2106@gmail.com>
2016-12-04 18:48:11 +0330

    delete info.component

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component,NgZone  } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {InfoComponent} from './info.component'; 
 import {InfoProfile} from './infoProfile';
 
 @Component({
-   moduleId: module.id,
-   selector: 'my-app',
-   templateUrl: 'app.component.html',
-   styleUrls: ['app.component.css']
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   infoProfile:InfoProfile = {name:'',email:''};
@@ -28,16 +26,16 @@ export class AppComponent {
     this.fShowInfo = true;
     var profile = googleUser.getBasicProfile();
        // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-       // console.log('Full Name: ' + profile.getName());
-       // console.log('Given Name: ' + profile.getGivenName());
-       // console.log('Family Name: ' + profile.getFamilyName());
-       // console.log("Image URL: " + profile.getImageUrl());
-       // console.log("Email: " + profile.getEmail());
+       console.log('Full Name: ' + profile.getName());
+       console.log('Given Name: ' + profile.getGivenName());
+       console.log('Family Name: ' + profile.getFamilyName());
+       console.log("Image URL: " + profile.getImageUrl());
+       console.log("Email: " + profile.getEmail());
        this.zone.run(() => { this.infoProfile.name = profile.getName(),
                              this.infoProfile.email = profile.getEmail(),
                              console.log(profile)
                              
-       })   
+                           })   
   };
   signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -1,20 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { InfoComponent }   from './info.component';
 import { RouterModule, Routes } from '@angular/router';
 import {Component, Directive, Input, ViewChild,NgZone} from '@angular/core';
 
 @NgModule({
-  imports:      [ BrowserModule,
-  RouterModule.forRoot([
-  {
-    path: 'login',
-    component: InfoComponent
-  }
-])
-],
-  declarations: [ AppComponent,InfoComponent ],
+  imports:      [ BrowserModule],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 
